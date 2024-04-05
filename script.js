c9a69ef99b44806bb728cccbe41bb28999500a32
devjunaeid <junaeid.md48@gmail.com>
2024-04-05 20:14:09 +0600

    Script to process JSON data into Bar Chart.

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-import data from "./data.json"
+import data from "./data.json" assert { type: "json" };
 
-// Expenses Data.
-var weeklyExpenses = data;
-console.log(data)
+const chartStore = document.querySelector(".chart");
 
+/**
+ * Retuns HTML Templete based on json date.
+ * @param {JSON} data
+ * Ex. data = [
+ *  {
+ *  "day": @param {string},
+ *  "amaount": @param {float}
+ * }
+ * ]
+ */
+function generateChartTemplate(data) {
+  data.forEach((element) => {
+    const height = `"height: ${element.amount}%"`
+    const bar = 
+    `<div class="barCont">
+          <p class="barName">${element.day}</p>
+          <div class="bar">
+            <div class="barLine" style=${height}>
+              <span class="hide">${element.amount}</span>
+            </div>
+          </div>
+    </div>`;
+    chartStore.insertAdjacentHTML("beforeend", bar)
+  });
+}
+
+generateChartTemplate(data);
